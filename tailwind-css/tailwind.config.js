dd3b54cfdb27c57a415feee56d1b9d6df61bcad3
readwanmd <readwanmd@gmail.com>
2023-12-10 11:15:37 +0600

    Tailwind init

---
+++ tailwind-css/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./pages/**/*.{html,js}', './components/**/*.{html,js}'],
+	theme: {
+		extend: {},
+	},
+	darkMode: 'class', // 'media or class'
+	plugins: [],
+};
